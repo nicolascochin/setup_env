69c434b3b267ab18d7f19fcaf8676b328693b1c7
Nicolas COCHIN <github@account.rocks>
2025-07-02 20:43:13 +0200

    Update distrobox.zsh

--- zsh/.zshrc.d/distrobox.zsh
+++ zsh/.zshrc.d/distrobox.zsh
@@ -1,12 +1,11 @@
-
 linux_dev() {
   local action="$1"
   shift
   bash <(curl -fsSL https://raw.githubusercontent.com/nicolascochin/distrobox/main/$action) "$@"
 }
- 
+
 list_distroboxes() {
-  (distrobox ls | awk -F '|' 'NR > 1 {print $2}'  && ls -1 $HOME/Distroboxes) | tr -d ' ' | sort -u
+  ls -1 $HOME/Distroboxes | tr -d ' ' | sort -u
 }
 
 _comp_distros() {
@@ -22,24 +21,30 @@ dev_create() {
     return 1
   fi
 
-  case $(uname -s) in 
-    Linux)
-      linux_dev "create" --name $name --image debian:latest --package figlet --package fuse --docker
+  case $(uname -s) in
+   Linux)
+      linux_dev create --name $name --image debian:latest --package figlet --package fuse --docker
       ;;
+    Darwin)
+      lima -- zsh -lic "linux_dev create --name $name --image debian:latest --package figlet --package fuse --docker --host_home /Users/$(whoami)"
+;;
   esac
 }
 
 dev_enter() {
   local name="$1"
-  shift 
+  shift
 
   if [[ -z "$name" ]]; then
     echo "Error: No name was given"
     exit 1
   fi
-  case $(uname -s) in 
+  case $(uname -s) in
+    Darwin)
+      lima -- zsh -lic "linux_dev enter $name --root"
+      ;;
     Linux)
-      linux_dev "enter" $name --root
+      linux_dev enter $name --root
       ;;
   esac
 }
@@ -47,15 +52,18 @@ compdef _comp_distros dev_enter
 
 dev_delete() {
   local name="$1"
-  shift 
+  shift
 
   if [[ -z "$name" ]]; then
     echo "Error: No name was given"
     exit 1
   fi
-  case $(uname -s) in 
+  case $(uname -s) in
+    Darwin)
+      lima -- zsh -lic "linux_dev delete $name --root"
+      ;;
     Linux)
-      linux_dev "delete" $name --root
+      linux_dev delete $name --root
       ;;
   esac
 }
